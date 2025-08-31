bbc049450b6378bbdc94fca00d07d9640dd8f2a3
Dylan.Seo <donghyseo@icloud.com>
2025-09-01 00:15:13 +0900

    31강까지 수강함. 2025-08-31

--- starting-project/src/components/PostsList.jsx
+++ starting-project/src/components/PostsList.jsx
@@ -1,12 +1,44 @@
+import { useState } from 'react';
+
 import Post from './Post';
+import NewPost from './NewPost';
+import Modal from './Modal';
 import classes from './PostsList.module.css';
 
+
 function PostsList() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
+  const [enteredBody, setEnteredBody] = useState('');
+  const [enteredAuthor, setEnteredAuthor] = useState('');
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
+
+
   return (
-    <ul className = {classes.posts}>
-      <Post author = "maximilan" body = "React.js is awesome!" />
-      <Post author = "Manuel" body = "Check out the full course!" />
-    </ul>
+    <>
+      {modalIsVisible ? (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal> 
+      ) : false}
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Manuel" body="Check out the full course!" />
+      </ul>
+    </>
   );
 }
 
